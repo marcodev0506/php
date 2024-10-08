37b23bc44d6b16bf01c59ca8da14e9fe4373a493
Marco Ocanto <mocanto@cnti.gob.ve>
2024-10-08 15:09:50 -0400

    concatenar

--- ejercicio.php
+++ ejercicio.php
@@ -1,2 +1,8 @@
 <?php
-echo "Contenido prueba ";
+echo "Contenido prueba \n \n \n";
+
+$Hotel = "Santana";
+$Playa = "Caribe";
+$Mensualidad= 2000;
+
+echo "El total a pagar es " . $Mensualidad; // El '.' es para concatenar en este caso un mensaje de salida con el valor de una variable 
